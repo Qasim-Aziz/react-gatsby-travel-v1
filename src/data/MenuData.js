514e38d7de7e350e4624c9ce943892def00debe7
qasim-aziz <envoyfacilitation@gmail.com>
2021-04-25 22:52:03 +0000

    initialcommit

---
+++ src/data/MenuData.js
@@ -0,0 +1,7 @@
+export const menuData = [
+    {title: "About", link: "/about"},
+    {title: "Trip", link: "/trips"},
+    {title: "Gallery", link: "/gallery"},
+    {title: "Contact", link: "/contact"},
+
+]
